f7ce3ab5dc5502c1287579d11a80bbc4531ad262
Eleazar Alfredo Alfriadez Yriarte <ealfriadez@gmail.com>
2026-08-18 16:34:24 -0500

    Vamor por el microservicio de carrito de comras

---
+++ microservices/cart-microservice/src/main/java/pe/edu/unfv/microservices/cartmicroservice/CartMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.microservices.cartmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartMicroserviceApplication.class, args);
+    }
+
+}
